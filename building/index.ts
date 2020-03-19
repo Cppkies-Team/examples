e56981e7dc65e0ae195d623a5b59ab4516fa1875
Lubomir <lubomir.yudenko@gmail.com>
2020-03-19 07:53:39 +0200

    Create shared.ts and link files

--- building/index.ts
+++ building/index.ts
@@ -1,15 +1,16 @@
 import Cppkies from "cppkies"
+import { prefix } from "../shared"
 Cppkies.onLoad.push(() => {
-	Cppkies.buildingLink = "TBA"
-	Cppkies.iconLink = "TBA"
+	Cppkies.buildingLink = `${prefix}/static/buildingBigIcon.png`
+	Cppkies.iconLink = `${prefix}/static/buildingIcons.png`
 	new Cppkies.Building(
 		"Think tank",
 		"think tank|think tanks|thought about|[X] extra neural pathway|[X] extra neural pathways",
 		"Generates cookies by thinking really, really hard about them.",
 		[0, 0],
 		{
-			bg: "TBA",
-			pic: "TBA",
+			bg: `${prefix}/static/buildingBg.png`,
+			pic: `${prefix}/static/buildingBrain.png`,
 			yV: 64,
 			xV: 16
 		},
@@ -18,4 +19,5 @@ Cppkies.onLoad.push(() => {
 		{ name: "Brain", desc: "Brain", icon: [1, 0] },
 		["Brainstorm", "Brain fart"]
 	)
+	//TODO: Add upgrades, achievements
 })
---
+++ shared.ts
@@ -0,0 +1,2 @@
+export const prefix =
+	"https://raw.githubusercontent.com/Cppkies-Team/examples/master"
